245a625991c33ecd62eb297196b8d88e8199b1e9
Víctor Bravo <vbravo.quintana@gmail.com>
2025-03-13 23:08:05 +0000

    Configurados el React Router DOM y el appContext - flux

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import injectContext from './js/store/appContext.js'
+
+const AppWithContext = injectContext(App);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
