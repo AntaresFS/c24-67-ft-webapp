0f61f0ebd9c66ec050d6ea3cbec091b4b171c058
John <juan_manuel_hg@hotmail.com>
2025-03-13 21:51:48 -0600

    Se modifica extension de archivo appContext.js -> appContext.jsx ya que generaba error al levantar el front

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import injectContext from './js/store/appContext.js'
+import injectContext from './js/store/appContext.jsx'
 
 const AppWithContext = injectContext(App);
 
